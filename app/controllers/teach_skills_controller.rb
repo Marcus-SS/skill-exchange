3905512272eb4681abf0254a86d3ff12e6f71e51
Marcus Blanchard <wotevenislif3@gmail.com>
2023-11-29 18:11:01 +0800

    added functionality to profile

--- app/controllers/teach_skills_controller.rb
+++ app/controllers/teach_skills_controller.rb
@@ -3,29 +3,41 @@ class TeachSkillsController < ApplicationController
     @teach_skills = TeachSkill.all
   end
 
+  def edit
+    @teach_skill = TeachSkill.find_by(id: params[:id])
+  end
+
+  def update
+    @teach_skill = TeachSkill.find_by(id: params[:id])
+    @teach_skill.update(teach_skill_params)
+    redirect_to profile_edit_path
+  end
 
   def new
     @teach_skill = TeachSkill.new
+    @teach_skills = TeachSkill.all
+    @skills = Skill.all
   end
 
   def create
     @teach_skill = TeachSkill.new(teach_skill_params)
+    @teach_skill.user = current_user
     if @teach_skill.save
-      redirect_to teach_skill_path(@teach_skill)
+      redirect_to profile_edit_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @teach_skill = TeachSkill.find_by(id: params[:id])
+    @teach_skill = TeachSkill.find(params[:id])
     @teach_skill.destroy
-    redirect_to teach_skills_path
+    redirect_to profile_edit_path
   end
 
   private
 
   def teach_skill_params
-    params.require(:teach_skill).permit(:user_id, :skill_id)
+    params.require(:teach_skill).permit(:skill_id, :mode, :level)
   end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,5 @@
 class Skill < ApplicationRecord
+  has_many :teach_skills
+  has_many :learn_skills
+
 end
