31a2942f1eb003d017d377e184048da78d5406f0
Marcus Blanchard <wotevenislif3@gmail.com>
2023-11-30 16:42:15 +0800

    created matching system

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,7 @@
 class MatchesController < ApplicationController
   def index
-    @matches = Match.all
+    @matches = find_matches(current_user)
+
   end
 
   def show
@@ -12,12 +13,7 @@ class MatchesController < ApplicationController
   end
 
   def create
-    @match = Match.new(match_params)
-    if @match.save
-      redirect_to match_path(@match)
-    else
-      render :new
-    end
+    find_matches(current_user)
   end
 
   def destroy
@@ -26,7 +22,65 @@ class MatchesController < ApplicationController
     redirect_to matches_path
   end
 
+
+
+
   private
+
+  def find_matches(user)
+    matches = []
+
+    User.where.not(id: user.id).each do |other_user|
+      if has_matching_skills?(other_user.teach_skills, user.learn_skills) &&
+         has_matching_skills?(other_user.learn_skills, user.teach_skills)
+        matches << create_match(user, other_user)
+      end
+    end
+
+    matches.uniq
+  end
+
+  def has_matching_skills?(skills1, skills2)
+    skills1.each do |skill1|
+      skills2.each do |skill2|
+        return true if skill1.skill_id == skill2.skill_id
+      end
+    end
+    false
+  end
+
+  def create_match(user, other_user)
+   # Find matching teach skill in user's teach skills
+   user_teach_skill = user.teach_skills.find { |teach_skill| has_matching_skills?([teach_skill], other_user.learn_skills) }
+
+   # Find matching learn skill in user's learn skills
+   user_learn_skill = user.learn_skills.find { |learn_skill| has_matching_skills?([learn_skill], other_user.teach_skills) }
+
+   # Find matching teach skill in other_user's teach skills
+   other_user_teach_skill = other_user.teach_skills.find { |teach_skill| has_matching_skills?([teach_skill], user.learn_skills) }
+
+   # Find matching learn skill in other_user's learn skills
+   other_user_learn_skill = other_user.learn_skills.find { |learn_skill| has_matching_skills?([learn_skill], user.teach_skills) }
+
+   # Create a match with the found skills
+   m = Match.new(
+     teach_skill_1_id: user_teach_skill&.id,
+     learn_skill_1_id: user_learn_skill&.id,
+     teach_skill_2_id: other_user_teach_skill&.id,
+     learn_skill_2_id: other_user_learn_skill&.id,
+      status: 'Pending'
+    )
+
+    if m.save
+      puts "Match created"
+      return m
+    else
+      puts "Failed to create match"
+      return nil
+    end
+  end
+
+
 
   def match_params
     params.require(:match).permit(:teach_skill_1_id, :learn_skill_1_id, :teach_skill_2_id, :learn_skill_2_id)
