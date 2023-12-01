94a3c6e27f9ca484779213650c96ed4d2c68e63f
Daniel Gonzalo <danicogonzalo@gmail.com>
2023-12-01 13:23:26 +0800

    action cable working

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :learn_skills
   resources :teach_skills
   resources :matches
-  resources :chatrooms, only: :show do
+  resources :chatrooms, only: [:index, :show] do
     resources :messages, only: :create
   end
   # resources :profiles
