f6381efa7fccd02f741e859c15379cc8bc272e6a
Marcus Blanchard <wotevenislif3@gmail.com>
2023-12-01 14:10:42 +0800

    added pfps

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   has_many :learn_skills
   has_many :teach_skills
   has_one_attached :photo
+
+  after_create :attach_default_photo
+
+  private
+
+  def attach_default_photo
+    # Check if a photo is attached during user creation
+    return if photo.attached?
+
+    # Attach the default photo (replace 'default_image.jpg' with your actual default image name)
+    photo.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'SkillExchange.png')), filename: 'SkillExchange.png', content_type: 'image/png')
+  end
 end
